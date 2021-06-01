0378afb6b41335f5177db4bb1f647bc592a7176f
dana-younis <younisdana7@gmail.com>
2021-06-01 16:26:40 +0300

    13

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const mongoose = require('mongoose');
 const UserModel = require('./module/user.js');
 
@@ -26,21 +27,74 @@ function homepage(req, res) {
 app.get('/books', getBooks);
 
 
+
+
+
 function getBooks(req, res) {
     const { email } = req.query;
     UserModel.find({ email: email },
 
-        function (err, data) {
+        (err, data) => {
             if (err) res.send('did not work');
             else {
-                // console.log(data);
-              res.send(data[0].books)
+                 console.log(data);
+                  res.send(data[0].books)
+
             }
         });
-    // console.log('email', email);
-    // console.log('email2',email2);
+
 
 }
+
+
+app.post('/books', createBooks);
+
+function createBooks(request, response) {
+    console.log(request.body);
+    const { email, bookName, bookDescription, bookStatus } = request.body;
+    UserModel.find({ email: email }, (error, data) => {
+        console.log(data);
+        data[0].books.push({
+            name: bookName,
+            description: bookDescription,
+            status: bookStatus
+        });
+        data[0].save();
+        response.send(data[0].books);
+    });
+}
+
+
+
+app.delete('/books/:index', deleteBooksForEmail);
+
+function deleteBooksForEmail(req, res) {
+
+    const index = Number(req.params.index);
+    console.log(req.params);
+
+    const { email } = req.query;
+    console.log(email);
+    UserModel.find({ email: email }, (err, data) => {
+
+        const newBooksArr = data[0].books.filter((user, idx) => {
+
+            if( idx !== index) return user;
+           
+        
+
+        });
+        data[0].books = newBooksArr;
+        data[0].save();
+
+        res.send(data[0].books);
+    });
+}
+
+
+
+
+
 
 
 
